b783b53579f82cb935154368bd580a1712a1d9ac
Kirill <mrbomberman99@gmail.com>
2021-05-31 22:01:44 +0300

    added some effects for webcamFun

--- webCamFun/script.js
+++ webCamFun/script.js
@@ -25,6 +25,17 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0 , 0, width, height) // помещае наше видео в контекст нужной области, задавая координаты начала и ширину и высоту картинки - тем самым переносим нужный нам масштаб в область побольше
+        // take the pixels out
+        let pixels = ctx.getImageData(0,0, width, height); // метод Canvas 2D API, возвращает объект ImageData, представляющий базовые пиксельные данные для области холста, обозначенного прямоугольником, который начинается в точке (sx, sy) и имеет ширину sw и высоту sh.
+        // выдает большой массив данных, в котором дается подробная информация о каждом пикселе нашей картинки из контекста
+        // mess with pixels
+        // pixels = redEffect(pixels);
+        // pixels = rggSplit(pixels);
+        // ctx.globalAlpha = 0.8;
+        pixels = greenScreen(pixels)
+        // put them back
+        ctx.putImageData(pixels, 0, 0)  // CanvasRenderingContext2D.putImageData() метод Canvas 2D API рисует данные из заданного ImageData объекта на холст. На этот метод не влияет матрица преобразования холста.
+        // помещаем наши измененные пиксели на всю страницу отображения видео -  в контекст
     }, 20) // выполняется каждый 20 миллисекунд
 }
 
@@ -44,6 +55,54 @@ function takePhoto() {
     strip.insertBefore(link, strip.firstChild); // вставляем ссылку на нашу фотографию в панель загрузки
 
 }
+
+function redEffect(pixels){ // увеличиваем насыщенность цветов, получая красный эффект
+    // loop every pixel we have
+    for(let i =0; i < pixels.data.length; i = i + 4){ // обращаемся к data - чтобы изменить цвет, благодаря getImageData
+        pixels.data[i] = pixels.data[i] + 75  // red
+        pixels.data[i+1] = pixels.data[i+1] - 50 // green
+        pixels.data[i+2] = pixels.data[i+2] * 0.5// blue
+    }
+    return pixels
+}
+
+function rggSplit(pixels){ // чтобы все цвета съезжали в разные стороны
+    for(let i =0; i < pixels.data.length; i = i + 4){ // обращаемся к data - чтобы изменить цвет, благодаря getImageData
+        pixels.data[i - 150] = pixels.data[i]  // red
+        pixels.data[i + 500] = pixels.data[i+1]  // green
+        pixels.data[i - 550] = pixels.data[i+2] // blue
+    }
+    return pixels
+}
+
+    function greenScreen(pixels) {
+        const levels = {}; // hold our min and max green
+      
+        document.querySelectorAll('.rgb input').forEach((input) => {
+          levels[input.name] = input.value; // получаем все наши ползунки и задаем каждому уровню текущее состояние, которое мы указываем на ползунке
+        }); // обращаемся к каждому уровню по имени, которое взяли у каждого ползунка из верстки
+      
+        for (i = 0; i < pixels.data.length; i = i + 4) {
+          red = pixels.data[i + 0];
+          green = pixels.data[i + 1];
+          blue = pixels.data[i + 2];
+          alpha = pixels.data[i + 3];
+      
+          if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // take it out!
+            pixels.data[i + 3] = 0;
+          }
+        }
+      
+        return pixels;
+      }
+      
+
 
 getVideo();
 
